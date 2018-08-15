cf40424af401736719d282f46485a76839834016
Colin Roe <colinkroe@gmail.com>
2018-08-15 22:15:28 +0200

    ASP.NET Core 2.1 MVC Application - Initial Commit
    I started with an empty application

---
+++ ColinsBakeryShop/Controllers/FeedbackController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ColinsBakeryShop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ColinsBakeryShop.Controllers
+{
+    public class FeedbackController : Controller
+    {
+        private readonly IFeedbackRepository _feedbackRepository;
+
+        public FeedbackController(IFeedbackRepository feedbackRepository)
+        {
+            _feedbackRepository = feedbackRepository;
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Feedback feedback)
+        {
+            _feedbackRepository.AddFeedback(feedback);
+            return RedirectToAction("FeedbackComplete");
+        }
+
+        public IActionResult FeedbackComplete()
+        {
+            return View();
+        }
+    }
+}
---
+++ ColinsBakeryShop/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ColinsBakeryShop.Models;
+using ColinsBakeryShop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ColinsBakeryShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+
+        public HomeController(IPieRepository pieRepository)
+        {
+            _pieRepository = pieRepository;
+        }
+
+        public IActionResult Index()
+        {
+            var pies = _pieRepository.GetAllPies().OrderBy(p => p.Name);
+
+            var homeViewModel = new HomeViewModel()
+            {
+                Title = "Welcome to Colin's Bakery Shop",
+                Pies = pies.ToList()
+            };
+
+            return View(homeViewModel);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var pie = _pieRepository.GetPieById(id);
+            if (pie == null)
+                return NotFound();
+
+            return View(pie);
+        }
+    }
+}
